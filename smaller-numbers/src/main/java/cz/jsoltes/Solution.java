22614aa1d35180c6a8226ee1b12808d5e356cd66
Juraj Soltes <jsoltes@redhat.com>
2016-01-14 16:03:59 +0100

    new project smaller-numbers

---
+++ smaller-numbers/src/main/java/cz/jsoltes/Solution.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.jsoltes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jsoltes
+ */
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> solution=new ArrayList<Integer>();
+        return solution;
+    }
+}
