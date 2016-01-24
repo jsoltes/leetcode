166e6e60d2dbcabaad0a2efe7a3a224cc9e6b2e0
Juraj Soltes <jsoltes@redhat.com>
2016-01-24 15:49:00 +0100

    almost complete only support for duplicate elements missing

--- smaller-numbers/src/main/java/cz/jsoltes/Solution.java
+++ smaller-numbers/src/main/java/cz/jsoltes/Solution.java
@@ -5,9 +5,7 @@
  */
 package cz.jsoltes;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -31,12 +29,12 @@ public class Solution {
         lower[len - 1] = -1;
         higher[len - 1] = -1;
         //the rest
+        int compared=-1;
         for (int i = len - 2; i >= 0; i--) {
             int elem = nums[i];
             count[i] = 0;
             updated[i] = 1;
-            int compared = -1;
-            int nextCompared = len - 1;
+            int nextCompared=len-1;
             while (nextCompared != -1) {
                 compared = nextCompared;
                 if (elem > nums[compared]) {
@@ -48,12 +46,17 @@ public class Solution {
                 }
             }
             if (nums[i] < nums[compared]) {
-                higher[i]=compared;
+                lower[i] = -1;
+                higher[i]=-1;
                 lower[compared]=i;
             } else {
-                lower[i]=compared;
+                higher[i] = -1;
+                lower[i]=-1;
                 higher[compared]=i;
             }
+        }
+        for(int i=0;i<len;i++){
+            System.out.println("elem "+nums[i]+" count "+count[i]+" updated "+updated[i]+" higher "+higher[i]+" lower "+lower[i]);
         }
         return Arrays.asList(count);
     }
@@ -61,7 +64,7 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println("zadanie  [26,78,27, 100,33, 67, 90,23, 66, 5, 38, 7, 35,23, 52,22, 83,51, 98, 69, 81,32, 78,28, 94,13, 2, 97, 3, 76,99,51, 9,21,84,66,65,36,100,41]");
-        System.out.println("result   " + s.countSmaller(new int[]{26, 78, 27, 100, 33, 67, 90, 23, 66, 5, 38, 7, 35, 23, 52, 22, 83, 51, 98, 69, 81, 32, 78, 28, 94, 13, 2, 97, 3, 76, 99, 51, 9, 21, 84, 66, 65, 36, 100, 41}));
+        System.out.println("result   " + s.countSmaller(new int[]{26,78,27, 100,33, 67, 90,23, 66, 5, 38, 7, 35,23, 52,22, 83,51, 98, 69, 81,32, 78,28, 94,13, 2, 97, 3, 76,99,51, 9,21,84,66,65,36,100,41}));
         System.out.println("expected [10, 27, 10, 35, 12, 22, 28, 8, 19, 2, 12, 2, 9, 6, 12, 5, 17, 9, 19, 12, 14, 6, 12, 5, 12, 3, 0, 10, 0, 7, 8, 4, 0, 0, 4, 3, 2, 0, 1, 0]");
     }
 }
