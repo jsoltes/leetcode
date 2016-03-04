f187a5074b4138232005ca2c13a15a2a8fe0fcff
Juraj Soltes <jsoltes@redhat.com>
2016-03-04 16:10:08 +0100

    first tests written

--- add-operations/src/test/java/cz/jsoltes/add/operations/SolutionTest.java
+++ add-operations/src/test/java/cz/jsoltes/add/operations/SolutionTest.java
@@ -5,41 +5,40 @@
  */
 package cz.jsoltes.add.operations;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.newArrayList;
 
 /**
  *
  * @author jsoltes
  */
 public class SolutionTest {
-    
-    public SolutionTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    Solution s = new Solution();
+
+    @Test
+    public void testAddOperators01() {
+        assertThat(s.addOperators("123", 6)).isEqualTo(newArrayList("1+2+3", "1*2*3"));
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testAddOperators02() {
+        assertThat(s.addOperators("232", 8)).isEqualTo(newArrayList("2*3+2", "2+3*2"));
+    }
+
+    @Test
+    public void testAddOperators03() {
+        assertThat(s.addOperators("105", 5)).isEqualTo(newArrayList("1*0+5", "10-5"));
+    }
+
+    @Test
+    public void testAddOperators04() {
+        assertThat(s.addOperators("00", 0)).isEqualTo(newArrayList("0+0", "0-0", "0*0"));
+    }
+
+    @Test
+    public void testAddOperators05() {
+        assertThat(s.addOperators("3456237490", 99)).isEqualTo(newArrayList());
+    }
 }
