8e78ccc99831a7b4a613519bedfa5d30e7ecd3c8
Juraj Soltes <jsoltes@redhat.com>
2016-03-04 15:51:49 +0100

    add-operations project set-up

---
+++ add-operations/src/main/java/cz/jsoltes/add/operations/Solution.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.jsoltes.add.operations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jsoltes
+ */
+public class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> solution = new ArrayList<>();
+        return solution;
+    }
+}
