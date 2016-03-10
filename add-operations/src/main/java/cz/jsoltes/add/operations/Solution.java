c71613ea56c0fb093121a7ac118ae59df9e73e27
Juraj Soltes <jsoltes@redhat.com>
2016-03-10 18:58:13 +0100

    first (not tested) solution for positive targets

--- add-operations/src/main/java/cz/jsoltes/add/operations/Solution.java
+++ add-operations/src/main/java/cz/jsoltes/add/operations/Solution.java
@@ -7,6 +7,8 @@ package cz.jsoltes.add.operations;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -19,30 +21,45 @@ import javax.script.ScriptException;
 public class Solution {
 
     //method that recursively generates all solutions for positive target
-    //TO-DO logic that builds the String solution
-    //TO-DO refine upper boundary in the for cycle
-    //TO-DO base case (num is empty)
-    private List<String> generatePositiveSolutions(String num, int len, int target, int result) {
-        List<String> solutions = new ArrayList<>();
-        List<String> signs = Arrays.asList("+", "-", "*");
-        int nextResult;
-        for (int i = 1; i < len; i++) {
-            nextResult=result+Integer.valueOf(num.substring(1,i+1));
-            solutions.addAll(generatePositiveSolutions(num.substring(i+1, len), len, target, nextResult));
-            
-            nextResult=result-Integer.valueOf(num.substring(1,i+1));
-            solutions.addAll(generatePositiveSolutions(num.substring(i+1, len), len, target, nextResult));
-            
-            nextResult=result*Integer.valueOf(num.substring(1,i+1));
-            solutions.addAll(generatePositiveSolutions(num.substring(i+1, len), len, target, nextResult));
+    //TODO generalize for negative target
+    private List<String> generatePositiveSolutions(String num, int len, int target, int result, StringBuilder solution) {
+        if (num.isEmpty()) {
+            if (result == target) {
+                return Arrays.asList(solution.toString());
+            } else {
+                return Collections.EMPTY_LIST;
+            }
+        } else {
+            List<String> solutions = new ArrayList<>();
+            int nextResult;
+            StringBuilder nextSolution;
+            for (int i = 1; i < len; i++) {
+                String nextString = num.substring(1, i + 1);
+                String theRest = num.substring(i + 1, len);
+                //a logic that decides that it is time to finish looping
+                int resultDigits = Integer.toString(result).length();
+                int nextDigits = nextString.length();
+                int restDigits = theRest.length();
+                int targetDigits = Integer.toString(target).length();
+                if (Math.max(resultDigits, nextDigits) <= Math.max(restDigits, targetDigits)) {
+                    //plus solutions
+                    nextResult = result + Integer.valueOf(nextString);
+                    nextSolution = solution.append("+").append(nextString);
+                    solutions.addAll(generatePositiveSolutions(theRest, len, target, nextResult, nextSolution));
+                    //minus solutions
+                    nextResult = result - Integer.valueOf(nextString);
+                    nextSolution = solution.append("-").append(nextString);
+                    solutions.addAll(generatePositiveSolutions(theRest, len, target, nextResult, nextSolution));
+                    //times solutions
+                    nextResult = result * Integer.valueOf(nextString);
+                    nextSolution = solution.append("*").append(nextString);
+                    solutions.addAll(generatePositiveSolutions(theRest, len, target, nextResult, nextSolution));
+                } else {
+                    break;
+                }
+            }
+            return solutions;
         }
-        return solutions;
-    }
-    
-    //method that recursively generates all solutions for negative target
-    private List<String> generateNegativeSolutions(String num, int len, int target, int result) {
-        List<String> solutions = new ArrayList<>();
-        return solutions;
     }
 
     public List<String> addOperators(String num, int target) {
@@ -56,16 +73,8 @@ public class Solution {
             //num is number higher than the target
         } else {
             int len = num.length();
-            if (target >= 0) {
-                return generatePositiveSolutions(num, len, target, num.charAt(0));
-            } else {
-                return generateNegativeSolutions(num, len, target, num.charAt(0));
-            }
+            StringBuilder sb = new StringBuilder(Character.toString(num.charAt(0)));
+            return generatePositiveSolutions(num, len, target, num.charAt(0), sb);
         }
-    }
-    public static void main(String[] args) throws ScriptException {
-        ScriptEngineManager sem = new ScriptEngineManager();
-        ScriptEngine se = sem.getEngineByName("JavaScript");
-        System.out.println(se.eval("3*2"));
     }
 }
